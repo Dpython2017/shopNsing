2d12924e0460342b86f2555bebe99db281960391
Dimple Mathew <dimplemathew@Dimples-MacBook-Air.local>
2021-01-24 20:44:00 +0530

    deploy setup

--- shopNsing/settings.py
+++ shopNsing/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'redis_cache',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
