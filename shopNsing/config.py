7cbb61be84508278104be2da8d4d391b68f01f67
Dimple Mathew <dimplemathew@Dimples-MacBook-Air.local>
2021-01-24 18:56:19 +0530

    deploy setup

--- shopNsing/config.py
+++ shopNsing/config.py
@@ -1,13 +1,12 @@
+import os
 
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
-        }
+         "BACKEND": "redis_cache.RedisCache",
+         "LOCATION": os.environ.get('REDIS_URL'),
     }
 }
+
 # Public API Config
 API_KEY = "4b32cb78da40ba12c2fea537c078ded6"
 MUSIXMATCH_URL = "https://api.musixmatch.com/ws/1.1/"
